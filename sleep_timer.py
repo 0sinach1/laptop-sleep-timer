27bd438ddfc8b350fde13d3be905303769d6ed62
0sinach1 <osimachifeanyi@gmail.com>
2026-01-20 22:32:46 +0100

    i have done my logic and now i want to improve on it

--- sleep_timer.py
+++ sleep_timer.py
@@ -4,6 +4,16 @@ import time
 file_path = r"C:\Users\Vinfotech\OneDrive\Documents\Coding\Projects\Python\SleepLaptop.bat"
 
 
+time_option = input('Do you want a me to sleep the laptop in seconds or in minutes?(s/m): ')
+
+if time_option == "s":
+    sec_time = int(input('In how many seconds do you me to sleep at?: '))
+elif time_option == "m":
+    min_time = int(input('In how many minutes do you me to sleep at?: '))
+else:
+    print('invalid option')
+
+
 is_counting = True
 
 def sleep_min(minutes):
@@ -26,8 +36,33 @@ def sleep_min(minutes):
             
         if (seconds == real_time):
             os.startfile(file_path)
+            break
+        
+        
+        
+sleep_min(min_time)
+
+
+
+def sleep_sec(seconds):
+    real_time = 0
+    
+    # print(f"this is minutes {min} and this is second {seconds}")  
+    # print(real_time)
+    # i = real_time - 1
+    
+    while is_counting:
+        time.sleep(1)
+        
+        if(seconds > real_time):
+            # print(f"time left: ")
+            seconds -= 1
+            print(f"{seconds}")  
             
+            
+        if (seconds == real_time):
+            os.startfile(file_path)
+            break
         
         
-        
-sleep_min(int(input("How many minutes before is sleep: ")))
+sleep_sec(sec_time)
