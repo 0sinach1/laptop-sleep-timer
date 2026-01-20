f511b5e4cd979944d505e8acedd0632e30cca1bb
0sinach1 <osimachifeanyi@gmail.com>
2026-01-20 22:14:10 +0100

    i made use of the time library and made a windows batch file for the main action

--- sleep_timer.py
+++ sleep_timer.py
@@ -1,31 +1,33 @@
 import os
 import time
 
+file_path = r"C:\Users\Vinfotech\OneDrive\Documents\Coding\Projects\Python\SleepLaptop.bat"
+
 
 is_counting = True
 
 def sleep_min(minutes):
     real_time = minutes * 60
     min = minutes
-    seconds = (minutes * 60) % 3600
+    seconds = 0
     
-    print(f"this is minutes {min} and this is second {seconds}")  
-    print(real_time)
-    i = real_time - 1
+    # print(f"this is minutes {min} and this is second {seconds}")  
+    # print(real_time)
+    # i = real_time - 1
     
     while is_counting:
         time.sleep(1)
         
-        if(i < real_time):
-            print(f"time left: {min}:{i}")
-            i -= 1
-            seconds = i % 60
-            print(f"this is minutes {min} and this is second {seconds}")  
+        if(seconds < real_time):
+            # print(f"time left: ")
+            seconds += 1
+            print(f"{seconds}")  
             
             
-        if (i == real_time):
-            os.openfile()
+        if (seconds == real_time):
+            os.startfile(file_path)
+            
         
         
         
-sleep_min(3)
+sleep_min(int(input("How many minutes before is sleep: ")))
